fe0acbcbc55d0f1c0ef4fb877ae5a84b53259754
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-07-30 21:43:50 +0600

    1005 - Average 1

---
+++ URI/Beginner/1005.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main()
+{
+  double x,y;
+  cin>>x>>y;
+  cout<<"MEDIA = "<<fixed<<setprecision(5)<<((3.5*x)+(7.5*y))/(3.5+7.5)<<"\n";
+  return 0;
+}
