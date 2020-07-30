c16f8cdb888affa5ee11a09a82941ed0de40af74
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-07-31 00:32:54 +0600

    1197 - Back to High School Physics

---
+++ URI/MATHEMATICS/1197.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define sp(a) fixed<<setprecision(a)
+using namespace std;
+using ll = long long int;
+typedef vector<ll> vi;
+int main()
+{
+	ll a,b;
+	while(cin>>a>>b){
+		cout<<(a*b*2)<<"\n";
+	}
+}
