562782238b7388b6d187770723355d66534d7312
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-07-31 02:13:49 +0600

    Pie Are Squared

---
+++ Toph/Pie_are_squared.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define sp(a) fixed<<setprecision(a)
+using namespace std;
+using ll = long long int;
+double r;
+void pi_oh_wow()
+{
+	cin>>r;
+	cout<<sp(10)<<acos(-1)*r*r<<"\n";
+}
+int main()
+{
+	pi_oh_wow();
+}
