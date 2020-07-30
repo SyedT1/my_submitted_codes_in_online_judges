c635b3d5271a39fea7d4dc01f57f29cc50477d1a
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-07-31 02:14:35 +0600

    Missing number

---
+++ Toph/Missing_num.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lo = unsigned long long int;
+lo k,a,b,c;
+void mn()
+{
+	cin>>k>>a>>b>>c;
+	cout<<k-(a+b+c)<<"\n";
+}
+int main()
+{
+	mn();
+}
