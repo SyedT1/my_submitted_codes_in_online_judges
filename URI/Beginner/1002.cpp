74b20cff03a30131728f66f145efdab7677ad072
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-07-30 21:40:37 +0600

    1002 - Area of a Circle

---
+++ URI/Beginner/1002.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define pi 3.14159
+using namespace std;
+using ll = long long int;
+int main()
+{
+	double r;
+	cin>>r;
+	cout<<"A="<<fixed<<setprecision(4)<<pi*r*r<<"\n";
+	return 0;
+}
