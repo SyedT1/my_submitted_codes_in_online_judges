70ab85e545ea412b23efbe7bd4353818657a6692
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 19:14:13 +0600

    The Boat Filled With Love

---
+++ Toph/The_Boat_Filled_With_Love.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+ll n;
+string k;
+void sypy(){
+  cin>>n;
+  cout<<"We Love You. We "<<n<<" Are Here For You.\n";
+}
+int main(){
+	sypy();
+}
