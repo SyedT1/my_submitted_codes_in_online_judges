abd86111f9fa9b58e3200989b56c77447898f44c
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 17:34:35 +0600

    Mixed Fractions

---
+++ Toph/Mixed_fractions.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lo = unsigned long long int;
+lo n,k,l;
+void mn()
+{
+	cin>>n>>k;
+	cout<<n/k<<" "<<n%k<<" "<<k<<"\n";
+}
+int main()
+{
+	mn();
+}
