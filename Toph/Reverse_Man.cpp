be54f69007a08d19dcb3ee0e7d07907dc6e5c52f
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 18:56:11 +0600

    Reverse Man

---
+++ Toph/Reverse_Man.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define all(a) (a).begin(),(a).end()
+using namespace std;
+using ll = long long int;
+string k;
+void rev(){
+	getline(cin,k);
+	reverse(all(k));
+	cout<<k<<"\n";
+}
+int main(){
+	rev();
+}
