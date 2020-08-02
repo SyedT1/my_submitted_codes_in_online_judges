796a35ad55da864b215bf7126b3176c06d545306
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 20:03:41 +0600

    Easy Summation

---
+++ Toph/Easy_Summation.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lo = unsigned long long int;
+lo n,k;
+int main(){
+	cin>>n;
+	for(lo i = 1;i<=n;i++){
+		cin>>k;
+		cout<<"Case "<<i<<": "<<k*k<<"\n";
+	}
+}
