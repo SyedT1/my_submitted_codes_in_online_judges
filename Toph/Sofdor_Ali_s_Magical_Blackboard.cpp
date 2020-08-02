9bdff4f80ffe01adef23bbbc35fc648f2a1e20b6
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 18:51:37 +0600

    Sofdor Ali's Magical Blackboard!

---
+++ Toph/Sofdor_Ali_s_Magical_Blackboard.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lo = unsigned long long int;
+lo n;
+void Con(){
+	lo a,b;
+	cin>>a>>b;
+	cout<<a*b<<"\n";
+}
+int main(){
+	cin>>n;
+	for(;n--;){
+		Con();
+	}
+}
