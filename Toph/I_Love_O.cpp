b6ca3527381b39d58a5e6db6ba2f1f6ba8bafcc5
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 19:25:11 +0600

    I Love 0

---
+++ Toph/I_Love_O.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using lo = unsigned long long int;
+lo n,k;
+void MoZa()
+{
+	cin>>n;
+	while(n--){
+		cin>>k;
+		cout<<k/10<<"\n";
+	}
+}
+int main()
+{
+	MoZa();
+}
