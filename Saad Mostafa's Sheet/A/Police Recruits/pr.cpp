660cc3fbed8dfc51556ab89db4c660059b3f313c
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-12-15 20:12:27 +0600

    Police Recruits

--- Saad Mostafa's Sheet/A/Police Recruits/pr.cpp
+++ Saad Mostafa's Sheet/A/Police Recruits/pr.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int n,p,unt = 0,sm = 0;cin>>n;while(n--){
-      cin>>p;
-      if(p>0){
-      	sm+=p;
-      }else{
-      	if(sm==0){
-            unt++;
-      	}else{
-      		if(sm>p){
-      			sm += p;
-      			p = 0;
-      		}else{
-      			p += sm;
-      			sm = 0;
-      		}
-      	}
-      }
+	freopen("input.txt", "r", stdin);
+	int n, p, unt = 0, sm = 0; cin >> n; while (n--) {
+		cin >> p;
+		if (p > 0) {
+			sm += p;
+		} else {
+			if (sm == 0) {
+				unt++;
+			} else {
+				if (sm > p) {
+					sm += p;
+					p = 0;
+				} else {
+					p += sm;
+					sm = 0;
+				}
+			}
+		}
 	}
-    cout<<unt<<'\n';
-    return 0;
+	cout << unt << '\n';
+	return 0;
 }
