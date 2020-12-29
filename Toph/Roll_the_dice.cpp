6c78ee25a81b2a12d0492ef3d75079d6a00900d5
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-12-29 12:51:15 +0600

    Roll The Dice

---
+++ Toph/Roll_the_dice.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+   //freopen("output.txt","w",stdout);
+   int n;cin>>n;
+   for(int i = 1,l;i<=n;i++){
+      cin>>l;cout<<l*6<<' '<<l<<'\n';
+   }
+}
