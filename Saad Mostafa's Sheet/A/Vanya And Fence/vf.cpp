feb70cdf1705aae7856c01e766ec93045808ed14
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-03-12 14:17:11 +0600

    Vanya and Fence

--- Saad Mostafa's Sheet/A/Vanya And Fence/vf.cpp
+++ Saad Mostafa's Sheet/A/Vanya And Fence/vf.cpp
@@ -1,18 +1,12 @@
-#include "iostream"
-#include "string"
-#include "vector"
-#include "queue"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
 using namespace std;
-using ll = long long int;
-void TakeIO() {
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-}
 int main() {
-//	TakeIO();
-	int n, h, wdh = 0, inp; cin >> n >> h; for (int i = 0; i < n; i++) {
-		cin >> inp, wdh += (inp <= h ? 1 : 2);
+	int n, h, s = 0, l; cin >> n >> h; for (int i = 0; i < n; i++) {
+		cin >> l, s += 1+ (l > h);
 	}
-	cout << wdh << '\n';
+	cout << s << '\n';
 	return 0;
 }
